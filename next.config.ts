1c92c2cb9c8574bdc4d493ae362bc26b39330307
jayOnTheStreet <taychakhun.r@gmail.com>
2025-05-08 03:20:14 +0700

    feat: implement admin login page with role verification

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,17 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-   /* config options here */
-   reactStrictMode: true,
-   images: {
-      remotePatterns: [
-         {
-            protocol: 'https',
-            hostname: 'etletpzpbuxirovvaths.supabase.co',
-            pathname: '/storage/v1/object/public/**',
-         },
-      ],
-   },
-};
+  /* config options here */
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'etletpzpbuxirovvaths.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
