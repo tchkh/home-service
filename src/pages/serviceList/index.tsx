197ea2e8be93ff35ea73d8cd7dbda789a2f6f821
Charlee2543 <165679195+Charlee2543@users.noreply.github.com>
2025-05-08 21:05:31 +0700

    style:set style service card layout display and mobile

--- src/pages/serviceList/index.tsx
+++ src/pages/serviceList/index.tsx
@@ -1,86 +1,48 @@
-// import { Prompt } from 'next/font/google';
+import { Prompt } from 'next/font/google';
 import ServiceCard from '@/components/ServiceCard';
+import axios from 'axios';
 
-// const prompt = Prompt({
-//    subsets: ['latin', 'thai'],
-//    weight: ['300', '400', '500', '600'],
-// });
+const prompt = Prompt({
+   subsets: ['latin', 'thai'],
+   weight: ['300', '400', '500', '600'],
+});
 
 interface ServiceCardProps {
    id: number;
-   title: string;
-   image: string;
-   category: string;
-   minPrice: string;
-   maxPrice: string;
+   category_name: string;
+   service_title: string;
+   image_url: string;
+   min_price: string;
+   max_price: string;
 }
+export const getServerSideProps = async () => {
+   // เป็นการ render ฝั่ง server ก่อน ตามด้วย runฝั่ง client
+   // getServerSideProps เป็น function ที่ run บน next เท่านั้น
+   //ค่าที่ return จะถูกส่งเข้าไปเป็น props ของ component หน้า
+   const res = await axios.get('http://localhost:3000/api/service');
+   return {
+      props: {
+         services: res.data,
+      },
+   };
+};
 
-const services: ServiceCardProps[] = [
-   {
-      id: 1,
-      title: 'ช่างแอร์',
-      image: 'https://etletpzpbuxirovvaths.supabase.co/storage/v1/object/public/service-image//clean_air_conditioner.jpg',
-      category: 'กรุงเทพมหานคร',
-      minPrice: '500฿ - 1,000฿',
-      maxPrice: '500฿ - 1,000฿',
-   },
-   {
-      id: 2,
-      title: 'ช่างไฟฟ้า',
-      image: 'https://etletpzpbuxirovvaths.supabase.co/storage/v1/object/public/service-image//install_exhaust_hood.jpg',
-      category: 'กรุงเทพมหานคร',
-      minPrice: '500฿ - 800฿',
-      maxPrice: '500฿ - 800฿',
-   },
-   {
-      id: 3,
-      title: 'ช่างประปา',
-      image: 'https://etletpzpbuxirovvaths.supabase.co/storage/v1/object/public/service-image//install_air_cinditioner.jpg',
-      category: 'กรุงเทพมหานคร',
-      minPrice: '450฿ - 900฿',
-      maxPrice: '450฿ - 900฿',
-   },
-   {
-      id: 4,
-      title: 'ช่างทาสีภายใน',
-      image: 'https://etletpzpbuxirovvaths.supabase.co/storage/v1/object/public/service-image//install_exhaust_hood.jpg',
-      category: 'กรุงเทพมหานคร',
-      minPrice: '500฿ - 1,200฿',
-      maxPrice: '500฿ - 1,200฿',
-   },
-   {
-      id: 5,
-      title: 'ช่างซ่อมเครื่องซักผ้า',
-      image: 'https://etletpzpbuxirovvaths.supabase.co/storage/v1/object/public/service-image//install_exhaust_hood.jpg',
-      category: 'กรุงเทพมหานคร',
-      minPrice: '400฿ - 800฿',
-      maxPrice: '400฿ - 800฿',
-   },
-   {
-      id: 6,
-      title: 'ช่างซ่อมเตาแก๊ส',
-      image: 'https://etletpzpbuxirovvaths.supabase.co/storage/v1/object/public/service-image//install_exhaust_hood.jpg',
-      category: 'กรุงเทพมหานคร',
-      minPrice: '300฿ - 700฿',
-      maxPrice: '300฿ - 700฿',
-   },
-];
-export default function Home() {
+export default function Home({ services }: { services: ServiceCardProps[] }) {
    return (
-      <>
-         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6  ">
+      <div className={prompt.className}>
+         <div className="bg-[var(--gray-200)] pb-14 grid grid-cols-1 justify-items-center  md:pt-[60px] md:px-[160px] md:pb-[133px] md:grid-cols-3 md:gap-y-[48px]  md:gap-x-[37px] ">
             {services.map((service) => (
                <ServiceCard
                   key={service.id}
                   id={service.id}
-                  title={service.title}
-                  image={service.image}
-                  category={service.category}
-                  minPrice={service.minPrice}
-                  maxPrice={service.maxPrice}
+                  title={service.service_title}
+                  image={service.image_url}
+                  category={service.category_name}
+                  minPrice={service.min_price}
+                  maxPrice={service.max_price}
                />
             ))}
          </div>
-      </>
+      </div>
    );
 }
