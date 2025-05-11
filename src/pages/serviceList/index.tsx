336bda2f15ac5fd9617f06bf51bb6ce9ab8af839
Charlee2543 <165679195+Charlee2543@users.noreply.github.com>
2025-05-12 01:42:32 +0700

    fix:change run content serviceList run server to run client

--- src/pages/serviceList/index.tsx
+++ src/pages/serviceList/index.tsx
@@ -1,41 +1,52 @@
 import { Prompt } from 'next/font/google';
 import ServiceCard from '@/components/ServiceCard';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const prompt = Prompt({
    subsets: ['latin', 'thai'],
    weight: ['300', '400', '500', '600'],
 });
 
-interface ServiceCardProps {
-   id: number;
-   category_name: string;
-   service_title: string;
-   image_url: string;
-   min_price: string;
-   max_price: string;
-}
+// export const getServerSideProps = async () => {
+//    // เป็นการ render ฝั่ง server ก่อน ตามด้วย runฝั่ง client
+//    // getServerSideProps เป็น function ที่ run บน next เท่านั้น
+//    //ค่าที่ return จะถูกส่งเข้าไปเป็น props ของ component หน้า
+//    const res = await axios.get('http://localhost:3000/api/service');
 
-export const getServerSideProps = async () => {
-   // เป็นการ render ฝั่ง server ก่อน ตามด้วย runฝั่ง client
-   // getServerSideProps เป็น function ที่ run บน next เท่านั้น
-   //ค่าที่ return จะถูกส่งเข้าไปเป็น props ของ component หน้า
-   const res = await axios.get('http://localhost:3000/api/service');
+//    return {
+//       props: {
+//          services: res.data,
+//       },
+//    };
+// };
 
-   return {
-      props: {
-         services: res.data,
-      },
+export default function Home() {
+   interface ServiceCardProps {
+      id: number;
+      category_name: string;
+      service_title: string;
+      image_url: string;
+      min_price: string;
+      max_price: string;
+   }
+
+   const [dataCard, setServiceCard] = useState<ServiceCardProps[]>([]);
+
+   const getDataService = async () => {
+      const res = await axios.get('http://localhost:3000/api/service');
+      setServiceCard(res.data);
    };
-};
 
-export default function Home({ services }: { services: ServiceCardProps[] }) {
+   useEffect(() => {
+      getDataService();
+   }, []);
    return (
       <div
          className={`${prompt.className} flex items-center justify-center bg-[var(--gray-200)] `}
       >
          <div className="max-w-[1440px] grid grid-cols-1 justify-items-center px-3 pt-6 pb-14 md:pt-[60px] md:px-[160px] md:pb-[133px] gap-y-6 gap-x-4 md:grid-cols-3 md:gap-y-[48px]  md:gap-x-[37px] ">
-            {services.map((service) => (
+            {dataCard.map((service) => (
                <ServiceCard
                   key={service.id}
                   id={service.id}
