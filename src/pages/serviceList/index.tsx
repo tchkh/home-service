127bac590b41ac35b8608537a396ff114b63fdbc
Charlee2543 <165679195+Charlee2543@users.noreply.github.com>
2025-05-11 23:40:52 +0700

    style:change content to center edit marjin between crad

--- src/pages/serviceList/index.tsx
+++ src/pages/serviceList/index.tsx
@@ -15,11 +15,13 @@ interface ServiceCardProps {
    min_price: string;
    max_price: string;
 }
+
 export const getServerSideProps = async () => {
    // เป็นการ render ฝั่ง server ก่อน ตามด้วย runฝั่ง client
    // getServerSideProps เป็น function ที่ run บน next เท่านั้น
    //ค่าที่ return จะถูกส่งเข้าไปเป็น props ของ component หน้า
    const res = await axios.get('http://localhost:3000/api/service');
+
    return {
       props: {
          services: res.data,
@@ -29,8 +31,10 @@ export const getServerSideProps = async () => {
 
 export default function Home({ services }: { services: ServiceCardProps[] }) {
    return (
-      <div className={prompt.className}>
-         <div className="bg-[var(--gray-200)] pb-14 grid grid-cols-1 justify-items-center  md:pt-[60px] md:px-[160px] md:pb-[133px] md:grid-cols-3 md:gap-y-[48px]  md:gap-x-[37px] ">
+      <div
+         className={`${prompt.className} flex items-center justify-center bg-[var(--gray-200)] `}
+      >
+         <div className="max-w-[1440px] grid grid-cols-1 justify-items-center px-3 pt-6 pb-14 md:pt-[60px] md:px-[160px] md:pb-[133px] gap-y-6 gap-x-4 md:grid-cols-3 md:gap-y-[48px]  md:gap-x-[37px] ">
             {services.map((service) => (
                <ServiceCard
                   key={service.id}
