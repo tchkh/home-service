6b41e004e752bcc68d7f0937fbaaa6c8e38e9b1c
Charlee2543 <165679195+Charlee2543@users.noreply.github.com>
2025-05-12 11:18:34 +0700

    feat:add API to fillter search category min-max Price sortBy

--- src/pages/api/service.ts
+++ src/pages/api/service.ts
@@ -7,11 +7,47 @@ export default async function handler(
 ) {
    if (req.method === 'GET') {
       try {
-         const { data: services, error } = await supabase
+         const { search, category, minPrice, maxPrice, sortBy } = req.query as {
+            search?: string;
+            category?: string;
+            minPrice?: string;
+            maxPrice?: string;
+            sortBy?: string;
+         };
+         console.log('req.query :  ', req.query);
+
+         let query = supabase
+
             .from('services_with_card')
             .select('*')
-            .range(0, 8)
-            .order('id', { ascending: true });
+            .range(0, 8);
+
+         if (search) {
+            query = query.ilike('service_title', `%${search}%`);
+         }
+
+         if (category) {
+            query = query.eq('category_name', category);
+         }
+
+         if (minPrice) {
+            query = query.gte('min_price', minPrice);
+         }
+
+         if (maxPrice) {
+            query = query.lte('max_price', maxPrice);
+         }
+
+         if (sortBy === 'price_asc') {
+            query = query.order('min_price', { ascending: true });
+         } else if (sortBy === 'price_desc') {
+            query = query.order('min_price', { ascending: false });
+         } else if (sortBy === 'title') {
+            query = query.order('id', { ascending: true });
+         }
+
+         const { data: services, error } = await query;
+
          if (error) {
             console.log('error: ', error);
             return res.status(500).json({ error: 'Error fetching services' });
