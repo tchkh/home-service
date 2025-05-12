b80fb92b5338e25d9e71945a9a08aa16a1ccbbf0
Charlee2543 <165679195+Charlee2543@users.noreply.github.com>
2025-05-13 02:43:57 +0700

    feat:add elment search content input buton onClick to search data , install icon frontawesome uo icon search

--- src/pages/serviceList/index.tsx
+++ src/pages/serviceList/index.tsx
@@ -2,11 +2,20 @@ import { Prompt } from 'next/font/google';
 import ServiceCard from '@/components/ServiceCard';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const prompt = Prompt({
    subsets: ['latin', 'thai'],
    weight: ['300', '400', '500', '600'],
 });
+
+const iconSearch = (
+   <FontAwesomeIcon
+      className="text-[var(--gray-300)]"
+      icon={faMagnifyingGlass}
+   />
+);
 
 /* 
 เก็บใน useState เป็น object
@@ -52,6 +61,7 @@ export default function Home() {
       sortBy: 'title',
       onLimit: null,
    });
+   const [inputSearch, setInputSearch] = useState<string>('');
 
    // URLSearchParams จะแปลง object เป็น  ?minPrice='500'&?maxPrice=`4000`
    const queryString = new URLSearchParams({
@@ -62,6 +72,13 @@ export default function Home() {
       sortBy: dataQuery.sortBy,
       onLimit: (dataQuery.onLimit ?? '').toString(),
    }).toString();
+
+   const inputTextSearch = () => {
+      setDataQuery((prevState) => ({
+         ...prevState,
+         search: inputSearch,
+      }));
+   };
 
    const getDataService = async () => {
       try {
@@ -82,7 +99,35 @@ export default function Home() {
       <div
          className={`${prompt.className} flex flex-col  items-center bg-[var(--gray-200)]`}
       >
-         <div className="max-w-[1440px] grid grid-cols-1 justify-items-center px-3 pt-6 pb-14 md:pt-[60px] md:px-[160px] md:pb-[133px] gap-y-6 gap-x-4 md:grid-cols-3 md:gap-y-[48px]  md:gap-x-[37px] ">
+         {/* ส่วน search bar */}
+         <section className="w-[375px]  h-[134px] bg-[var(--white)] flex flex-col p-4 gap-y-4 ">
+            <div className={`flex  gap-x-4 w-full`}>
+               <div className="relative w-full ">
+                  <span className="absolute left-4 top-1/2 transform -translate-y-1/2">
+                     {iconSearch}
+                  </span>
+                  <input
+                     type="text"
+                     placeholder="ค้นหารายการ..."
+                     className=" text-body-2 pl-10 border-2 border-[var(--gray-300)] min-w-[240px] w-full h-[45px] rounded-lg  placeholder:text-[16px] placeholder:text-[var(--gray-700)] " // เพิ่ม padding ด้านซ้ายเพื่อให้มีพื้นที่สำหรับ icon
+                     value={inputSearch}
+                     onChange={(e) => {
+                        setInputSearch(e.target.value);
+                     }}
+                  />
+               </div>
+               <button
+                  type="button"
+                  onClick={inputTextSearch}
+                  className="btn btn--primary w-[85px] h-[45px]"
+               >
+                  ค้นหา
+               </button>
+            </div>
+            <div className="border-amber-600 border-2"></div>
+         </section>
+         {/* ส่วน card */}
+         <section className="max-w-[1440px] grid grid-cols-1 justify-items-center px-3 pt-6 pb-14 md:pt-[60px] md:px-[160px] md:pb-[133px] gap-y-6 gap-x-4 md:grid-cols-3 md:gap-y-[48px]  md:gap-x-[37px] ">
             {dataCard.map((service) => (
                <ServiceCard
                   key={service.id}
@@ -94,7 +139,7 @@ export default function Home() {
                   maxPrice={service.max_price}
                />
             ))}
-         </div>
+         </section>
       </div>
    );
 }
