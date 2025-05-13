45a1af949154ed0fd62ce950095bd6e01b3d350f
Prachaya Kosolpattrapk <prachaya.ksp@gmail.com>
2025-05-13 16:49:36 +0700

    feat(middleware): add authRedirect middleware and utils to restrict access to login/register pages for authenticated users
    - Create `middleware/authRedirect.ts` to prevent logged-in users from accessing /login and /register
    - Add `middleware/utils` to check current user's session using Supabase

---
+++ src/lib/middleware/authRedirect.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { Database } from "@/types/supabase";
+
+
+export async function authRedirectMiddleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient<Database>({ req, res });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  const url = req.nextUrl.clone();
+  if (session && ["/register", "/login"].includes(url.pathname)) {
+    url.pathname = "/";
+    return NextResponse.redirect(url);
+  }
+
+  return res;
+}
+
+
+
