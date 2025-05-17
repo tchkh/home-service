36052773cbffc46a486238bce7dc8d2d9f01f0b5
Charlee2543 <165679195+Charlee2543@users.noreply.github.com>
2025-05-17 19:05:13 +0700

    feat:add API searchService for search Auto complete

---
+++ src/pages/api/searchService.ts
@@ -0,0 +1,41 @@
+import supabase from "@/lib/supabase";
+import type { NextApiRequest, NextApiResponse } from "next";
+export default async function handler(
+   req: NextApiRequest,
+   res: NextApiResponse
+) {
+   /* 
+   หากไม่มี query เข้ามา ไม่ให้ fetch api
+   หากไม่พบเนื้อหา ให้ ส่งข้อมูลว่า none service
+   */
+   try {
+      const { searchTitle } = req.query as { searchTitle: string };
+      // console.log("searchTitle: ", searchTitle);
+      if (searchTitle) {
+         const { data: services, error } = await supabase
+            .from("services")
+            .select("title")
+            .range(0, 4)
+            .ilike("title", `%${searchTitle}%`);
+         // console.log("data: ", services);
+         if (error) {
+            // console.log("error: ", error);
+            return res.status(500).json({ error: "Error fetching services" });
+         }
+         if (services[0]) {
+            return res.status(200).json(services);
+         } else {
+            // หาไม่มีข้มูลให้ return
+            return res
+               .status(404)
+               .json("Sorry, this service is not available.");
+         }
+      } else {
+         //หากไม่มีการใส้คำค้นหาให้ return
+         return res.status(400).json("Please enter a search term.");
+      }
+   } catch (error) {
+      // console.log("error: ", error);
+      return res.status(500).json({ error: `Error fetching services${error}` });
+   }
+}
