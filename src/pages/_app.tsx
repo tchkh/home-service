5e6df69fa5d0cc5c585dd10cf9ac292186c5e074
Prachaya Kosolpattrapk <prachaya.ksp@gmail.com>
2025-05-22 16:38:52 +0700

    feat: implement UserContext for user state management and refactor Navbar and LoginPage components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,25 +4,28 @@ import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
 import { useRouter } from "next/router";
 import { Prompt } from "next/font/google";
+import { UserProvider } from "@/contexts/UserContext";
 
 const prompt = Prompt({
-   subsets: ['latin', 'thai'],
-   weight: ['300', '400', '500', '600'],
+  subsets: ["latin", "thai"],
+  weight: ["300", "400", "500", "600"],
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-   const router = useRouter();
-   const navbarPages = ['/login', '/register', '/serviceList'];
-   const footerPages = ['/', '/serviceList'];
+  const router = useRouter();
+  const navbarPages = ["/login", "/register", "/serviceList"];
+  const footerPages = ["/", "/serviceList"];
 
-   const showNavbar = navbarPages.includes(router.pathname);
-   const showFooter = footerPages.includes(router.pathname);
+  const showNavbar = navbarPages.includes(router.pathname);
+  const showFooter = footerPages.includes(router.pathname);
 
-   return (
+  return (
+    <UserProvider>
       <main className={`${prompt.className}`}>
-         {showNavbar && <Navbar />}
-         <Component {...pageProps} />
-         {showFooter && <Footer />}
+        {showNavbar && <Navbar />}
+        <Component {...pageProps} />
+        {showFooter && <Footer />}
       </main>
-   );
+    </UserProvider>
+  );
 }
