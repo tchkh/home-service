42fb3aac243ce091ea33e4160974e6c5404c7fad
Charlee2543 <165679195+Charlee2543@users.noreply.github.com>
2025-05-30 03:29:41 +0700

    feat:add API category get(all ,by id ) ,post to create, put to edit ,delete

---
+++ src/lib/middleware/validateCategory.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest } from "next";
+
+// เช็คว่ามี name color ไหม
+export function validateCategory(req: NextApiRequest) {
+   const { name, color } = req.body;
+
+   if (!name || typeof name !== "string") {
+      return { error: "Missing or invalid 'name'" };
+   }
+
+   if (!color || typeof color !== "string") {
+      return { error: "Missing or invalid 'color'" };
+   }
+
+   return { error: null }; // ถ้าทุกอย่างถูกต้อง
+}
+
+export function checkIdCategory(req: NextApiRequest) {
+   const { id } = req.body;
+   if (!id) {
+      return { error: "Missing or invalid 'id'" };
+   }
+   return { error: null };
+}
