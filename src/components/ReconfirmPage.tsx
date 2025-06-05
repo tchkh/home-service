48a014f6838e244b1789812b1466fbf36b8a0ccd
Charlee2543 <165679195+Charlee2543@users.noreply.github.com>
2025-06-06 05:53:29 +0700

    feat:add click out content popup to hide popup

--- src/components/ReconfirmPage.tsx
+++ src/components/ReconfirmPage.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from "react";
+import React, { useRef, useEffect, Dispatch, SetStateAction } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
 
@@ -23,13 +23,29 @@ function ReconfirmPage({
       setTaggle(false);
    };
 
+   const boxRef = useRef<HTMLDivElement>(null); // สำหรับตรวจว่าคลิกนอกกล่องไหม
+
+   // ตรวจจับการคลิกนอกกล่อง
+
+   useEffect(() => {
+      const handleClickOutside = (event: MouseEvent) => {
+         if (boxRef.current && !boxRef.current.contains(event.target as Node)) {
+            setTaggle(false); // ถ้าคลิกนอกกล่อง input + box ให้ซ่อน
+         }
+      };
+      document.addEventListener("mousedown", handleClickOutside);
+      return () =>
+         document.removeEventListener("mousedown", handleClickOutside);
+   }, [setTaggle]);
+
    return (
       <div
          className={`${
             toggle ? "flex" : "hidden"
          } z-50 fixed w-[100%] h-[100vh] bg-[#00000034]  items-center justify-center `}
       >
-         <section
+         <div
+            ref={boxRef}
             className={`relative w-[360px] h-[270px] py-[36px] px-[36px] gap-2 bg-[var(--white)] flex flex-col items-center justify-center box-border rounded-[16px]`}
          >
             <div className="absolute right-0 top-0 w-[48px] h-[48px] flex items-center justify-center ">
@@ -63,7 +79,7 @@ function ReconfirmPage({
                   ยกเลิก
                </button>
             </div>
-         </section>
+         </div>
       </div>
    );
 }
